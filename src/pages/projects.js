c2127b7cbbbdda4347e5a14e14fc1ebe30b4ad47
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2020-03-27 23:54:12 -0400

    complete gatsby tutorial

---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import Layout from "../components/layout"
+import React from "react"
+import FancyParagraph from "../components/fancy-paragraph"
+
+
+const Projects = () => (
+    <Layout>
+        <h1>Projects</h1>
+        <FancyParagraph paragraphText="Styled with CSS Modules." />
+    </Layout>
+)
+export default Projects

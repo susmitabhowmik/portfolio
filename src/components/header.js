3f909038fb4210da51e5ab98d0c00c343f0a87d3
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2020-03-29 15:49:43 -0400

    feat (index.js): add about me section

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <h1>Oh hey there, I'm Susmita.</h1>
+    <h1>I'm Susmita, a software developer.</h1>
     <a href="/" class="header__link">Home</a> |
     <a href="/projects/" class="header__link"> Projects</a>
   </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,25 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Susmita Bhowmik" />
-    <div class="flip-box">
-      <div class="flip-box--inner">
-        <div class="flip-box--front">
-          <img src={SusmitaBitmoji} alt="picuture of me"></img>
-        </div>
-        <div class="flip-box--back">
-          <img src={Susmita} alt="picuture of me"></img>
+    <div class="homepage-wrapper">
+      <div class="flip-box">
+        <div class="flip-box--inner">
+          <div class="flip-box--front">
+            <img src={SusmitaBitmoji} alt="cartoon picuture of me"></img>
+          </div>
+          <div class="flip-box--back">
+            <img src={Susmita} alt="picuture of me"></img>
+          </div >
         </div >
       </div >
-    </div >
+      <h2 class="homepage__header">About Me</h2>
+      <p>
+        I am a full-stack web developer who is constantly working to get better at my craft. Currently, I work as an apprentice at a web development company called Sparkbox, learning as much as I can.
+        Outside of Sparkbox, I am completing NYU's Bridge to Tandon program to gain acceptance to NYU's masters in computer science program. Prior to working as a developer, I had worked as an elementary
+        school teacher. I use my skills gained from working as a teacher such as breaking down problems, communicating, and patience to make me a better software developer. When I am not coding, I spend my time reading, traveling, and
+        drinking lots of coffe.
+      </p>
+    </div>
     <p>Welcome to your new Gatsby site.</p>
     <Link to="/projects/">See my work.</Link>
   </Layout >

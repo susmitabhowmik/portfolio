76092e76e0e3744a29bcc645fb7abd5acb7e590f
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2020-03-31 12:12:54 -0400

    feat: update header to include routes

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header>
     <h1>I'm Susmita, a software developer.</h1>
+    <a href="/" class="header__link">Home</a> |
+    <a href="/projects/" class="header__link"> Projects</a>
   </header>
 )
 

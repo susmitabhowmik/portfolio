d505ca67cc304ac7f96ee2e78b449099dcedeb41
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2020-04-28 21:54:24 -0400

    update link color

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,11 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header>
     <h1>I'm Susmita, a software developer.</h1>
-    <a href="/" class="header__link">Home</a> |
-    <a href="/projects/" class="header__link"> Projects</a> |
-    <a href="/blogIndex/" class="header__link"> Blog</a>
+    <div class="header__links">
+      <a href="/" class="header__link">Home</a> |
+      <a href="/projects/" class="header__link"> Projects</a> |
+      <a href="/blogIndex/" class="header__link"> Blog</a>
+    </div>
   </header>
 )
 

f15c3d4246a1dc49933fbcb4275f7a9bf9aac64d
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2020-04-28 21:38:20 -0400

    update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
